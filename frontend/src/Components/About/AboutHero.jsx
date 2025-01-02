f64406ac7d2b7a5bac2bba8befbd6fd541d154e3
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-01-02 10:20:06 +0530

    dark gradient when hovering about page images

--- frontend/src/Components/About/AboutHero.jsx
+++ frontend/src/Components/About/AboutHero.jsx
@@ -12,9 +12,12 @@ const AboutHero = () => {
 
     return (
         <div className='mt-40'>
-            <div className='w-11/12 mx-auto  space-y-4 md:space-y-0 md:flex md:space-x-4'>
+            <div className='w-11/12 mx-auto  grid gap-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 '>
                 {products.map((product) => (
-                    <div key={product.id} className='h-119 xl:h-160 lg:w-1/3 xl:w-1/2 relative'>
+                    <div key={product.id} className='h-119 xl:h-160 lg:w-full relative group'>
+                        {/* Gradient Overlay */}
+                        <div className="absolute inset-0 bg-gradient-to-b from-black/40 via-black/60 to-black/80 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-xl"></div>
+
                         <div className="absolute top-32 space-y-2 lg:top-20 xl:top-56 left-0 w-10/12 h-full  text-white flex flex-col justify-center  p-4 rounded-xl">
                             <h2 className="text-2xl   xl:text-3xl font-Switzer-Medium">{product.title}</h2>
                             <p className="text-sm xl:text-base font-Switzer-Light text-white">{product.details}</p>
