83fddbd3a35cebb590e458d84854a921ffb63929
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-03-17 16:03:53 +0530

    frontend jobListing integrated with backend

--- frontend/src/Components/Careers/JobListings.jsx
+++ frontend/src/Components/Careers/JobListings.jsx
@@ -1,67 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getJobsApi } from "../../utils/api/careerApi";
 
 const JobListings = () => {
-  const jobs = [
-    {
-      title: "Junior Designer",
-      type: "Full-time",
-      location: "Calicut",
-      description: `We are looking for a talented Junior Designer to join our dynamic team. You will be responsible for creating visually compelling and user-friendly designs for our digital platforms. You will work closely with our marketing, development, and product teams to bring our brand to life through innovative and engaging digital experiences.`,
-      responsibilities: [
-        "Conceptualize and design engaging digital content for websites, social media, email campaigns, and advertisements.",
-        "Collaborate with cross-functional teams to ensure cohesive visual language across all digital touchpoints.",
-        "Develop wireframes, prototypes, and high-fidelity mockups for new website features and enhancements.",
-        "Maintain and update design assets, ensuring consistency with our brand guidelines.",
-        "Conduct user research and usability testing to inform design decisions.",
-      ],
-      qualifications: [
-        "Proven experience as a Digital Designer or in a similar role.",
-        "Bachelor's degree in Graphic Design, Digital Media, or a related field.",
-        "Proficiency in design software such as Adobe Creative Suite (Photoshop, Illustrator, XD).",
-      ],
-    },
-    { title: "Senior Strategist", type: "Full-time", location: "Calicut",description: `We are looking for a talented Junior Designer to join our dynamic team. You will be responsible for creating visually compelling and user-friendly designs for our digital platforms. You will work closely with our marketing, development, and product teams to bring our brand to life through innovative and engaging digital experiences.`,
-        responsibilities: [
-          "Conceptualize and design engaging digital content for websites, social media, email campaigns, and advertisements.",
-          "Collaborate with cross-functional teams to ensure cohesive visual language across all digital touchpoints.",
-          "Develop wireframes, prototypes, and high-fidelity mockups for new website features and enhancements.",
-          "Maintain and update design assets, ensuring consistency with our brand guidelines.",
-          "Conduct user research and usability testing to inform design decisions.",
-        ],
-        qualifications: [
-          "Proven experience as a Digital Designer or in a similar role.",
-          "Bachelor's degree in Graphic Design, Digital Media, or a related field.",
-          "Proficiency in design software such as Adobe Creative Suite (Photoshop, Illustrator, XD).",
-        ], },
-    { title: "Senior Designer", type: "Full-time", location: "Calicut",description: `We are looking for a talented Junior Designer to join our dynamic team. You will be responsible for creating visually compelling and user-friendly designs for our digital platforms. You will work closely with our marketing, development, and product teams to bring our brand to life through innovative and engaging digital experiences.`,
-        responsibilities: [
-          "Conceptualize and design engaging digital content for websites, social media, email campaigns, and advertisements.",
-          "Collaborate with cross-functional teams to ensure cohesive visual language across all digital touchpoints.",
-          "Develop wireframes, prototypes, and high-fidelity mockups for new website features and enhancements.",
-          "Maintain and update design assets, ensuring consistency with our brand guidelines.",
-          "Conduct user research and usability testing to inform design decisions.",
-        ],
-        qualifications: [
-          "Proven experience as a Digital Designer or in a similar role.",
-          "Bachelor's degree in Graphic Design, Digital Media, or a related field.",
-          "Proficiency in design software such as Adobe Creative Suite (Photoshop, Illustrator, XD).",
-        ], },
-    { title: "Design Director", type: "Full-time", location: "Calicut",description: `We are looking for a talented Junior Designer to join our dynamic team. You will be responsible for creating visually compelling and user-friendly designs for our digital platforms. You will work closely with our marketing, development, and product teams to bring our brand to life through innovative and engaging digital experiences.`,
-        responsibilities: [
-          "Conceptualize and design engaging digital content for websites, social media, email campaigns, and advertisements.",
-          "Collaborate with cross-functional teams to ensure cohesive visual language across all digital touchpoints.",
-          "Develop wireframes, prototypes, and high-fidelity mockups for new website features and enhancements.",
-          "Maintain and update design assets, ensuring consistency with our brand guidelines.",
-          "Conduct user research and usability testing to inform design decisions.",
-        ],
-        qualifications: [
-          "Proven experience as a Digital Designer or in a similar role.",
-          "Bachelor's degree in Graphic Design, Digital Media, or a related field.",
-          "Proficiency in design software such as Adobe Creative Suite (Photoshop, Illustrator, XD).",
-        ], },
-  ];
-
+  // const jobs = [
+  //   {
+  //     title: "Junior Designer",
+  //     type: "Full-time",
+  //     location: "Calicut",
+  //     description: `We are looking for a talented Junior Designer to join our dynamic team. You will be responsible for creating visually compelling and user-friendly designs for our digital platforms. You will work closely with our marketing, development, and product teams to bring our brand to life through innovative and engaging digital experiences.`,
+  //     responsibilities: [
+  //       "Conceptualize and design engaging digital content for websites, social media, email campaigns, and advertisements.",
+  //       "Collaborate with cross-functional teams to ensure cohesive visual language across all digital touchpoints.",
+  //       "Develop wireframes, prototypes, and high-fidelity mockups for new website features and enhancements.",
+  //       "Maintain and update design assets, ensuring consistency with our brand guidelines.",
+  //       "Conduct user research and usability testing to inform design decisions.",
+  //     ],
+  //     qualifications: [
+  //       "Proven experience as a Digital Designer or in a similar role.",
+  //       "Bachelor's degree in Graphic Design, Digital Media, or a related field.",
+  //       "Proficiency in design software such as Adobe Creative Suite (Photoshop, Illustrator, XD).",
+  //     ],
+  //   },
+  //   { title: "Senior Strategist", type: "Full-time", location: "Calicut",description: `We are looking for a talented Junior Designer to join our dynamic team. You will be responsible for creating visually compelling and user-friendly designs for our digital platforms. You will work closely with our marketing, development, and product teams to bring our brand to life through innovative and engaging digital experiences.`,
+  //       responsibilities: [
+  //         "Conceptualize and design engaging digital content for websites, social media, email campaigns, and advertisements.",
+  //         "Collaborate with cross-functional teams to ensure cohesive visual language across all digital touchpoints.",
+  //         "Develop wireframes, prototypes, and high-fidelity mockups for new website features and enhancements.",
+  //         "Maintain and update design assets, ensuring consistency with our brand guidelines.",
+  //         "Conduct user research and usability testing to inform design decisions.",
+  //       ],
+  //       qualifications: [
+  //         "Proven experience as a Digital Designer or in a similar role.",
+  //         "Bachelor's degree in Graphic Design, Digital Media, or a related field.",
+  //         "Proficiency in design software such as Adobe Creative Suite (Photoshop, Illustrator, XD).",
+  //       ], },
+  //   { title: "Senior Designer", type: "Full-time", location: "Calicut",description: `We are looking for a talented Junior Designer to join our dynamic team. You will be responsible for creating visually compelling and user-friendly designs for our digital platforms. You will work closely with our marketing, development, and product teams to bring our brand to life through innovative and engaging digital experiences.`,
+  //       responsibilities: [
+  //         "Conceptualize and design engaging digital content for websites, social media, email campaigns, and advertisements.",
+  //         "Collaborate with cross-functional teams to ensure cohesive visual language across all digital touchpoints.",
+  //         "Develop wireframes, prototypes, and high-fidelity mockups for new website features and enhancements.",
+  //         "Maintain and update design assets, ensuring consistency with our brand guidelines.",
+  //         "Conduct user research and usability testing to inform design decisions.",
+  //       ],
+  //       qualifications: [
+  //         "Proven experience as a Digital Designer or in a similar role.",
+  //         "Bachelor's degree in Graphic Design, Digital Media, or a related field.",
+  //         "Proficiency in design software such as Adobe Creative Suite (Photoshop, Illustrator, XD).",
+  //       ], },
+  //   { title: "Design Director", type: "Full-time", location: "Calicut",description: `We are looking for a talented Junior Designer to join our dynamic team. You will be responsible for creating visually compelling and user-friendly designs for our digital platforms. You will work closely with our marketing, development, and product teams to bring our brand to life through innovative and engaging digital experiences.`,
+  //       responsibilities: [
+  //         "Conceptualize and design engaging digital content for websites, social media, email campaigns, and advertisements.",
+  //         "Collaborate with cross-functional teams to ensure cohesive visual language across all digital touchpoints.",
+  //         "Develop wireframes, prototypes, and high-fidelity mockups for new website features and enhancements.",
+  //         "Maintain and update design assets, ensuring consistency with our brand guidelines.",
+  //         "Conduct user research and usability testing to inform design decisions.",
+  //       ],
+  //       qualifications: [
+  //         "Proven experience as a Digital Designer or in a similar role.",
+  //         "Bachelor's degree in Graphic Design, Digital Media, or a related field.",
+  //         "Proficiency in design software such as Adobe Creative Suite (Photoshop, Illustrator, XD).",
+  //       ], },
+  // ];
+  const [jobs, setJobs] = useState([])
   const [selectedJob, setSelectedJob] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getJobsApi();
+      setJobs(response.careers)
+    }
+
+    fetchPosts()
+  }, [])
 
   return (
     <div className="relative">
@@ -88,43 +98,66 @@ const JobListings = () => {
 
       {/* Job Details Panel */}
       {selectedJob && (
-        <div className="fixed top-0 right-0 h-full w-full md:w-1/3 bg-white shadow-lg p-6 overflow-y-auto">
+        <div className="fixed top-0 right-0 h-full w-full md:w-1/3 bg-white shadow-2xl p-6 overflow-y-auto transition-transform transform translate-x-0">
+          {/* Close Button */}
           <button
-            className="absolute top-4 right-4 text-gray-600"
+            className="absolute top-4 right-4 text-gray-500 hover:text-black text-2xl"
             onClick={() => setSelectedJob(null)}
           >
             ×
           </button>
-          <h2 className="text-xl font-Switzer-Medium mb-4">{selectedJob.title}</h2>
-          <p className="text-sm  mb-4">
-            {selectedJob.type} ({selectedJob.location})
-          </p>
-          <p className=" mb-6">{selectedJob.description}</p>
-          <h3 className="text-lg font-Switzer-Medium mb-2">Key Responsibilities</h3>
-          <ul className="list-disc list-inside mb-6">
-            {selectedJob.responsibilities?.map((item, index) => (
-              <li key={index} className="text-gray-600">
-                {item}
-              </li>
-            ))}
-          </ul>
-          <h3 className="text-lg font-Switzer-Medium mb-2">Qualifications</h3>
-          <ul className="list-disc list-inside mb-6">
-            {selectedJob.qualifications?.map((item, index) => (
-              <li key={index} className="">
-                {item}
-              </li>
-            ))}
-          </ul>
-          <button className='bg-gray1 font-Switzer-Medium text-xs mt-4 rounded-xl h-8 md:h-10 p-2 px-4 text-white'
-            onClick={() =>
-              (window.location.href = "mailto:hr@ficaads.com?subject=Application for " + selectedJob.title)
-            }
-          >
-            Apply
-          </button>
+
+          <div className="flex items-center h-full">
+          <div className="">
+            {/* Job Title */}
+            <h2 className="text-2xl font-bold text-gray-900 mb-2">{selectedJob.title}</h2>
+
+            {/* Employment Type & Location */}
+            <p className="text-sm text-gray-600 font-medium mb-4">
+              🏢 {selectedJob.employmentType} | 📍 {selectedJob.location}
+            </p>
+
+            {/* Description */}
+            <p className="text-gray-700 leading-relaxed mb-6">{selectedJob.description}</p>
+
+            {/* Responsibilities */}
+            {selectedJob.responsibilities && selectedJob.responsibilities.length > 0 && (
+              <>
+                <h3 className="text-lg font-semibold text-gray-900 mb-2">📌 Key Responsibilities</h3>
+                <ul className="list-disc list-inside text-gray-700 space-y-2 mb-6">
+                  {selectedJob.responsibilities.map((item, index) => (
+                    <li key={index} className="pl-2">{item}</li>
+                  ))}
+                </ul>
+              </>
+            )}
+
+            {/* Qualifications */}
+            {selectedJob.qualifications && selectedJob.qualifications.length > 0 && (
+              <>
+                <h3 className="text-lg font-semibold text-gray-900 mb-2">🎓 Qualifications</h3>
+                <ul className="list-disc list-inside text-gray-700 space-y-2 mb-6">
+                  {selectedJob.qualifications.map((item, index) => (
+                    <li key={index} className="pl-2">{item}</li>
+                  ))}
+                </ul>
+              </>
+            )}
+
+            {/* Apply Button */}
+            <button
+              className="w-full bg-black/20 text-gray1 font-medium text-sm md:text-base rounded-lg h-10 px-4 transition-transform transform hover:scale-105"
+              onClick={() =>
+                (window.location.href = `mailto:hr@ficaads.com?subject=Application for ${selectedJob.title}`)
+              }
+            >
+              Apply Now 🚀
+            </button>
+          </div>
+          </div>
         </div>
       )}
+
     </div>
   );
 };
