9e4c97c57c5fcb6eebbc3b40a8a40d2b3e344999
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-03-21 14:40:28 +0530

    spelling changed

--- frontend/src/Components/Careers/Internship.jsx
+++ frontend/src/Components/Careers/Internship.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import employee from "../../assets/Images/internship.webp"
+import employee from "../../assets/Images/Internship.webp"
 import { Link } from 'react-router-dom'
 
 const Internship = () => {
