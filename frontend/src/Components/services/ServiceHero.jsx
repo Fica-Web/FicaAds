d6756e8215f460cecfc3e2dae9345ab823480bdc
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-03-27 12:54:03 +0530

    service hero updated

--- frontend/src/Components/services/ServiceHero.jsx
+++ frontend/src/Components/services/ServiceHero.jsx
@@ -13,7 +13,7 @@ const ServiceHero = ({ service }) => {
 
     return (
         <div
-            className="relative w-full bg-black mb-40 flex flex-col items-center lg:flex-row lg:items-center lg:justify-between px-6 lg:px-20"
+            className="w-full bg-black mb-40 flex flex-col-reverse items-center lg:flex-row lg:items-center lg:justify-between px-3 lg:px-20"
             style={{
                 height: `${grayHeight}px`,
                 backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.8)), url(${image})`,
@@ -21,26 +21,25 @@ const ServiceHero = ({ service }) => {
                 backgroundPosition: "center",
             }}
         >
-            {/* Service Title - Positioned differently based on screen size */}
-            <div className="text-center lg:text-right mt-6 lg:mt-0 lg:absolute lg:right-20 lg:top-1/2 lg:-translate-y-1/2">
-                <h2 className="text-4xl md:text-5xl font-bold text-gray5 uppercase mt-5">
-                    {service.title}
-                </h2>
-            </div>
 
             {/* Gray Content Box */}
             <div
                 ref={grayDivRef}
-                className="w-[90%] max-w-2xl bg-gray5 border-b border-gray5 p-7 sm:p-10 lg:p-20 absolute sm:top-20 top-32
-                      left-1/2 -translate-x-1/2 
-                      lg:left-20 lg:translate-x-0"
+                className="w-full lg:w-1/2 max-w-2xl bg-gray5 border-b border-gray5 p-7 sm:p-10 lg:p-20 relative top-28 lg:mx-5"
             >
-                <h2 className="text-3xl md:text-4xl font-extrabold text-brandGreen font-Switzer-Medium mb-4 uppercase">
+                <h2 className="text-2xl md:text-4xl font-extrabold text-brandGreen font-Switzer-Medium mb-4 uppercase">
                     {service.subTitle}
                 </h2>
                 <p className="text-lg text-gray1 mx-auto mb-6">
                     {service.details}
                 </p>
+            </div>
+            
+            {/* Service Title - Positioned differently based on screen size */}
+            <div className="lg:w-1/2 w-full mt-6 lg:mt-0 relative lg:static top-28">
+                <h2 className="text-4xl md:text-5xl font-bold text-gray5 uppercase mt-8 text-center">
+                    {service.title}
+                </h2>
             </div>
         </div>
     );
