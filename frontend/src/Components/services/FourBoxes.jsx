dc453bc51ca572855671767ea4dde2fc01f4dc4c
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-03-28 14:30:44 +0530

    colors added fourBox component

--- frontend/src/Components/services/FourBoxes.jsx
+++ frontend/src/Components/services/FourBoxes.jsx
@@ -7,28 +7,28 @@ const FourBoxes = ({ section }) => {
         <div className="w-full bg-black min-h-[90vh] flex justify-center items-center my-20 p-20 relative">
             {/* Background Image Container with White Overlay */}
             <div
-                className="w-full min-h-[90vh] bg-cover bg-center bg-no-repeat -rotate-1 relative"
+                className="w-full min-h-[90vh] bg-cover bg-center bg-no-repeat -rotate-3 relative"
                 style={{ backgroundImage: `url(${bgImage})` }}
             >
                 {/* White Shade Overlay */}
                 <div className="absolute inset-0 bg-white/40"></div>
 
                 {/* Content */}
-                <div className='flex flex-1 justify-center items-center min-h-[90vh]'>
+                <div className='flex flex-1 justify-center items-center min-h-[90vh] p-20 rotate-3'>
                     {/* Content Container */}
                     <div className="relative grid grid-cols-2 bg-white shadow-xl max-w-4xl">
                         {/* Title Section */}
                         <div className="col-span-2 md:col-span-1 flex flex-col justify-center p-14 bg-gray">
                             <h2 className="text-4xl lg:text-6xl font-bold font-Switzer-Medium max-w-[350px] text-black mb-4 uppercase">{section?.heading}</h2>
-                            <p className="text-gray-700">{section?.description}</p>
+                            <p className="text-gray1">{section?.description}</p>
                         </div>
 
                         {/* Principle Boxes */}
                         {section?.subSections?.map((section, index) => (
-                            <div key={index} className="bg-gray-100 p-14 border border-gray5 flex items-center">
+                            <div key={index} className="bg-gray-100 p-14 last:bg-gray2 flex items-center">
                                 <div className=''>
-                                    <h3 className="text-xl font-semibold text-black">{section.subHeading}</h3>
-                                    <p className="text-gray-600 mt-2">{section.subDescription}</p>
+                                    <h3 className="text-3xl font-bold text-gray1">{section.subHeading}</h3>
+                                    <p className="text-lightgray mt-2">{section.subDescription}</p>
                                 </div>
                             </div>
                         ))}
