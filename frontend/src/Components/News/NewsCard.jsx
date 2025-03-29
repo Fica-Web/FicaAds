3d9598e9aa45d02e95624b55289a6df32fbddfd5
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-03-29 14:26:50 +0530

    newsCard component updated

--- frontend/src/Components/News/NewsCard.jsx
+++ frontend/src/Components/News/NewsCard.jsx
@@ -24,19 +24,23 @@ const NewsCard = () => {
   };
 
   return (
-    <div className="mt-20 animate-fade-up duration-1000 mb-10">
-      <div className="w-11/12 mx-auto">
-        <h1 className="text-2xl uppercase font-Switzer-Medium md:text-4xl lg:text-5xl">
-          Latest Blogs
-        </h1>
-      </div>
+    <>
+      {blogs.length > 0 && (
+        <div className="mt-20 animate-fade-up duration-1000 mb-10">
+          <div className="w-11/12 mx-auto">
+            <h1 className="text-2xl uppercase font-Switzer-Medium md:text-4xl lg:text-5xl">
+              Latest Blogs
+            </h1>
+          </div>
 
-      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-11/12 mx-auto">
-        {blogs.map((blog) => (
-          <SingleBlog key={blog._id} blog={blog} onClick={() => handleBlogClick(blog)} />
-        ))}
-      </div>
-    </div>
+          <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-11/12 mx-auto">
+            {blogs.map((blog) => (
+              <SingleBlog key={blog._id} blog={blog} onClick={() => handleBlogClick(blog)} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
