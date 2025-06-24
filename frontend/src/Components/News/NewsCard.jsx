f71a2d7ca5cd55ede59f442a1e45f3b49ab24ac5
Fica-Web <keralasummit@gmail.com>
2025-06-24 11:24:57 +0530

    setup

--- frontend/src/Components/News/NewsCard.jsx
+++ frontend/src/Components/News/NewsCard.jsx
@@ -15,7 +15,7 @@ const NewsCard = () => {
       } catch (error) {
         console.error("Error fetching blogs:", error);
       }
-    };
+    }; 
     fetchBlogs();
   }, []);
 
@@ -25,7 +25,7 @@ const NewsCard = () => {
 
   return (
     <>
-      {blogs.length > 0 && (
+      {blogs?.length > 0 && (
         <div className="mt-20 animate-fade-up duration-1000 mb-10">
           <div className="w-11/12 mx-auto">
             <h1 className="text-2xl uppercase font-Switzer-Medium md:text-4xl lg:text-5xl">
