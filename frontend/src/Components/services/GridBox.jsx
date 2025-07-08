421cff00f609277ce7b7cfe407bc16bde103fba9
Fica-Web <keralasummit@gmail.com>
2025-07-08 11:35:06 +0530

    grid box list item ui updated

--- frontend/src/Components/services/GridBox.jsx
+++ frontend/src/Components/services/GridBox.jsx
@@ -4,19 +4,19 @@ const GridBox = ({ section }) => {
     return (
         <div className="my-28">
             {/* Heading */}
-            <h2 className="text-3xl md:text-4xl lg:text-5xl font-Switzer-Regular font-semibold mb-12 text-black leading-tight">
+            <h2 className="text-3xl md:text-4xl lg:text-5xl font-Switzer-Regular font-semibold mb-4 text-black leading-tight">
                 {section.heading}
             </h2>
 
             {/* Optional Description */}
             {section.description && (
-                <p className="text-lg text-gray-700 text-center max-w-3xl mx-auto mb-10">
+                <p className="text-lg text-gray-700 ">
                     {section.description}
                 </p>
             )}
 
             {/* Grid of Advantage Sections */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
                 {section.subSections.map((sub, index) => (
                     <div
                         key={index}
@@ -26,7 +26,10 @@ const GridBox = ({ section }) => {
                             {sub.subHeading}
                         </h3>
 
-                        <ul className="text-lg space-y-4 text-gray1 list-disc list-outside pl-6">
+                        <ul
+                            className={`text-lg space-y-4 text-gray1 pl-6 ${sub.subDescription.length === 1 ? 'list-none' : 'list-disc list-outside'
+                                }`}
+                        >
                             {sub.subDescription.map((desc, i) => (
                                 <li key={i}>
                                     {desc.content}
