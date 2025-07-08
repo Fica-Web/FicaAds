97f1a4449bac4483edbe70874d184dc33923e334
Fica-Web <keralasummit@gmail.com>
2025-07-08 14:44:12 +0530

    mobile view navbar ui updated

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -26,15 +26,17 @@ const NavBar = () => {
     { name: "Social Media Marketing", link: "/services/smm" },
   ];
 
+  const isMobile = typeof window !== "undefined" && window.innerWidth < 1024;
+
   return (
     <div className="w-full mx-auto h-16 md:h-20 bg-white md:px-8 lg:px-8 lg:pr-12 z-50 relative">
       <div className="flex items-center justify-between py-4 relative h-full">
-        {/* Logo Section */}
+        {/* Logo */}
         <div className="w-24 lg:w-22 flex-shrink-0">
           <img src={logo} alt="Fica Logo" />
         </div>
 
-        {/* Mobile Menu Toggle */}
+        {/* Mobile Toggle */}
         <div
           onClick={() => setOpen(!open)}
           className="text-2xl mr-5 cursor-pointer lg:hidden"
@@ -42,9 +44,9 @@ const NavBar = () => {
           {open ? <FaTimes className="text-black" /> : <FaBars className="text-black" />}
         </div>
 
-        {/* Navigation Links */}
+        {/* Nav Links */}
         <ul
-          className={`lg:flex lg:items-center text-black lg:pb-0 absolute lg:static bg-white lg:bg-transparent
+          className={`lg:flex lg:items-center text-black absolute lg:static bg-white lg:bg-transparent
             lg:z-auto z-20 left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in-out
             ${open ? "top-16 opacity-100" : "top-[-490px]"} lg:opacity-100 opacity-0`}
         >
@@ -53,7 +55,6 @@ const NavBar = () => {
 
             return (
               <React.Fragment key={item.name}>
-                {/* Main Nav Item */}
                 <li
                   className={`lg:ml-8 lg:text-sm xl:text-sm font-Switzer-Medium uppercase lg:my-0 my-7 ${item.name === "Contact" ? "lg:ml-10" : ""
                     }`}
@@ -85,31 +86,25 @@ const NavBar = () => {
                   )}
                 </li>
 
-                {/* Services Dropdown */}
+                {/* SERVICES dropdown, placed after ABOUT */}
                 {item.name === "About" && (
                   <li
                     className="relative lg:ml-8 lg:text-sm xl:text-sm font-Switzer-Medium uppercase lg:my-0 my-7 cursor-pointer"
-                    onMouseEnter={() => setShowServices(true)}
-                    onMouseLeave={() => setShowServices(false)}
+                    onMouseEnter={() => !isMobile && setShowServices(true)}
+                    onMouseLeave={() => !isMobile && setShowServices(false)}
                   >
-                    {/* Desktop Trigger */}
-                    <div className="flex gap-1 items-center cursor-pointer">
-                      <span className="relative group hidden lg:inline-block">
+                    <div
+                      className="flex gap-1 items-center"
+                      onClick={() => isMobile && setShowServices(!showServices)}
+                    >
+                      <span className="relative group">
                         Services
                         <span className="absolute left-0 bottom-[-2px] h-0.5 bg-black transition-all duration-500 w-0 group-hover:w-full"></span>
                       </span>
-                      <IoMdArrowDropdown />
+                      <IoMdArrowDropdown className={`transition-transform ${showServices ? "rotate-180" : "rotate-0"}`} />
                     </div>
 
-                    {/* Mobile Trigger */}
-                    <span
-                      onClick={() => setShowServices(!showServices)}
-                      className="lg:hidden block"
-                    >
-                      Services
-                    </span>
-
-                    {/* Dropdown Menu */}
+                    {/* Dropdown */}
                     <AnimatePresence>
                       {showServices && (
                         <motion.ul
@@ -117,13 +112,14 @@ const NavBar = () => {
                           animate={{ opacity: 1, y: 0 }}
                           exit={{ opacity: 0, y: -10 }}
                           transition={{ duration: 0.2 }}
-                          className="absolute lg:top-full lg:left-0 mt-3 w-60 bg-white shadow-xl z-50"
+                          className={`absolute lg:top-full lg:left-0 mt-3 w-60 bg-white shadow-xl z-50 overflow-hidden ${isMobile ? "static mt-4" : ""
+                            }`}
                         >
                           {services.map((service, i) => (
                             <motion.li
                               key={i}
                               whileHover={{ scale: 1.02 }}
-                              className="rounded-md transition"
+                              className="transition"
                             >
                               <Link
                                 to={service.link}
