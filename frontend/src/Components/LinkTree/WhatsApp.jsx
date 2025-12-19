2d0089aa2413dd899e58fba73a6c090d67c48f39
Fica-Web <keralasummit@gmail.com>
2025-12-19 16:00:52 +0530

    Phone number changed on linktree

--- frontend/src/Components/LinkTree/WhatsApp.jsx
+++ frontend/src/Components/LinkTree/WhatsApp.jsx
@@ -6,7 +6,7 @@ const WhatsApp = () => {
         <motion.a
             whileTap={{ scale: 0.97 }}
             whileHover={{ y: -2 }}
-            href="https://wa.me/9746199213?text=I%20want%20to%20apply"
+            href="https://wa.me/918086555918?text=I%20want%20to%20apply"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center justify-center gap-3 w-full
